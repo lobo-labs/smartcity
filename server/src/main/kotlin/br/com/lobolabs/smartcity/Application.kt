41c13cd9cb78db02afe06d4a9818cf0598930484
lobo-labs <loboneto@lobolabs.com.br>
2025-03-06 10:17:44 -0300

    config webapp deploy

--- server/src/main/kotlin/br/com/lobolabs/smartcity/Application.kt
+++ server/src/main/kotlin/br/com/lobolabs/smartcity/Application.kt
@@ -2,18 +2,21 @@ package br.com.lobolabs.smartcity
 
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
+import io.ktor.server.http.content.default
+import io.ktor.server.http.content.files
+import io.ktor.server.http.content.static
+import io.ktor.server.http.content.staticFiles
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
-fun main() {
-    embeddedServer(Netty, port = SERVER_PORT, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
 fun Application.module() {
     routing {
-        get("/") {
+        staticFiles(remotePath = "/", File("composeApp/build/dist/wasmJs/productionExecutable"))
+        get("/health") {
             call.respondText("Ktor: ${Greeting().greet()}")
         }
     }
